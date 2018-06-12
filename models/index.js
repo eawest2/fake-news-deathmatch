c3c8e8e381d97663404c4496545e3a7796be14dd
unknown <eawest2@gmail.com>
2018-06-11 20:11:40 -0400

    updated requirements

--- models/index.js
+++ models/index.js
@@ -7,6 +7,8 @@ var basename  = path.basename(__filename);
 var env       = process.env.NODE_ENV || 'development';
 var config    = require(__dirname + '/../config/config.json')[env];
 var db        = {};
+var passport = require('passport');
+var LocalStrategy = require('passport-local').Strategy;
 
 if (config.use_env_variable) {
   var sequelize = new Sequelize(process.env[config.use_env_variable], config);

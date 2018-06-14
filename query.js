194885a0fb00666493c7ab3bc039c6996f3fcd4e
Rob Martin <rob.martin.5758@gmail.com>
2018-06-14 14:01:08 -0400

    query.js

--- query.js
+++ query.js
@@ -1,15 +1,32 @@
 var db = require("./models");
 
 
-let data = db.Reviewer.findAll({
+let data = db.Article.findAll({
+    // where: {
+    //   id: 11
+    // },
     include: [{
-      model: db.Article,
-      through: {
-        // attributes: ['createdAt', 'startedAt', 'finishedAt'],
-        where: {reviewerId: 1}
-      }
-    }]
+      model: db.Review,
+      as: 'ArticleReviews',
+    }],
   })
-  .then(reviews => {console.log(reviews)});
 
-  
+  .then(articles => {
+    console.log(`ARTICLES ${articles}`)
+    console.log(JSON.stringify(articles)),
+      console.log(`START MAP`)
+    articles.map(article => (
+      console.log(article.dataValues.id),
+      console.log(article.dataValues.URL),
+      console.log(article.dataValues.title),
+      console.log(article.dataValues.description),
+      console.log("Reviews:"),
+      article.ArticleReviews.map(review => (
+        console.log(`Ratings: ${review.rating1} ${review.rating2} ${review.rating3}`),
+        console.log(`Comment: ${review.comment}`)
+      ))
+
+
+    ));
+
+  })

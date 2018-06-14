858a56b690e4edc42a137b324ae265217778842e
Rob Martin <rob.martin.5758@gmail.com>
2018-06-14 14:13:34 -0400

    query.js

--- query.js
+++ query.js
@@ -16,17 +16,19 @@ let data = db.Article.findAll({
     console.log(JSON.stringify(articles)),
       console.log(`START MAP`)
     articles.map(article => (
-      console.log(article.dataValues.id),
-      console.log(article.dataValues.URL),
-      console.log(article.dataValues.title),
-      console.log(article.dataValues.description),
+      console.log("-------------------------------------------------------"),
+      console.log(`ID: ${article.dataValues.id}`),
+      console.log(`Source: ${article.dataValues.sourceId}`),
+      console.log(`Author: ${article.dataValues.author}`),
+      console.log(`Title: ${article.dataValues.title}`),
+      console.log(`Description: ${article.dataValues.description}`),
+      console.log(`URL: ${article.dataValues.URL}`),
+      console.log(`image URL: ${article.dataValues.imageURL}`),
       console.log("Reviews:"),
       article.ArticleReviews.map(review => (
         console.log(`Ratings: ${review.rating1} ${review.rating2} ${review.rating3}`),
         console.log(`Comment: ${review.comment}`)
       ))
-
-
     ));
 
   })

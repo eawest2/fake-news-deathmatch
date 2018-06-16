de277ac9fe933f2b8dd4ea3f0af0f60de8e66db4
unknown <eawest2@gmail.com>
2018-06-15 21:20:04 -0400

    updated tree

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -3,17 +3,25 @@ var db = require("../models");
 var Article = require("../models/article.js");
 var Review = require("../models/review.js");
 var $ = require("jquery");
-
+require('es6-promise').polyfill();
+require('isomorphic-fetch');
 
 // Routes
 module.exports = function(app) {
     // Autopull new data from API sources, format, and then write to database. 
     app.post("/api/pull", function() {
             var sources = ["https://newsapi.org/v2/everything?domains=cnn.com&language=en&pagesize=100&sortBy=popularity&apiKey=71df48734adf49a9a56c2e893c8408f1", "https://newsapi.org/v2/everything?domains=foxnews.com&language=en&pagesize=100&sortBy=popularity&apiKey=71df48734adf49a9a56c2e893c8408f1", 'https://newsapi.org/v2/everything?domains=nytimes.com&language=en&pagesize=100&sortBy=popularity&apiKey=71df48734adf49a9a56c2e893c8408f1']
-            Promise.all(sources.map(source=> $.ajax({
-                url: source,
-                method: "GET"
-            })))
+            Promise.all(sources.map(source=> fetch(source).then(function(response) {
+                if (response.status >= 400) {
+                    throw new Error("Bad response from server");
+                }
+                return response.json();
+            })
+            //     $.ajax({
+            //     url: source,
+            //     method: "GET"
+            // })
+        ))
             .then(responses=>{
                 db.Article.findAll()
                 .then(articles=>{
@@ -23,9 +31,11 @@ module.exports = function(app) {
                     console.log(articleMap);
                     responses = [].concat(...responses);
                     responses = responses.reduce((accumulator, response)=>{
-                        if(articleMap[response.description] !== true){
+                        if(articleMap[responses.description] !== true){
                             accumulator.push(response);
                             return accumulator;
+                            console.log(accumulator)
+                            console.log(responses);
                         }
                     },[]);
                     return Promise.all(responses.map(response=>db.Article.create({
@@ -53,7 +63,7 @@ module.exports = function(app) {
             comment:''
         }).then(review=>{
             db.Article.findOne({
-                'where':{
+                where:{
                     'id':req.params.id
                 }
             }).then(article=>{
@@ -81,16 +91,53 @@ module.exports = function(app) {
 
     //aggregate reviews of individual sources
 
-    app.get("api/jumbo", function (req, res){
+    app.get("/api/jumbo", function (req, res){
 
-        db.Review.findAll9({
-            where: {
+        var promise1 = new Promise(function (req,res) {
+            var aggregates = [];
+            var sources = ["cnn", "fox-news", "the-new-york-times"]
+            for (i = 0; i <3; i++){
+            db.Article.findAll({
+                order: [
+                    ['sourceId']
+                ],
+                attributes: [
+                    'id', 'sourceId', 'author', 'title', 'description', 'ArticleReviews.rating1', 'ArticleReviews.rating2', 'ArticleReviews.rating3'
+                ],
+                include: [{
+                model: db.Review,
+                    as: 'ArticleReviews',
+                }],
+                where: {
+                    sourceId: sources[i]
+                }
+            }).then(average=>{
+                //console.log(average);
+                var sourceArr= [];
+                var promise2= new Promise(function (req,res) {
+                    for (i = 0; i < average.length; i++){
+                    //console.log(average[i].dataValues.ArticleReviews);
+                    var reviewValue = average[i].dataValues.ArticleReviews.length;
+                    var reviewArr = [];
+                    for (a = 0; a< reviewValue; a++){
+                        let rating = average[i].dataValues.ArticleReviews[a].dataValues
+                        var value = (Math.floor(10*(rating.rating1 + rating.rating2 + rating.rating3)/3))/10
+                        reviewArr.push(value);
+                    };
+                    //console.log(reviewArr);
+                    var finalScore = (Math.floor(10*((reviewArr[0] + reviewArr[1] + reviewArr[2])/3))/10);
+                    sourceArr.push(finalScore);
+                };
+                }).then(container=>{
+                    console.log(sourceArr);
+                });
 
-            }
-        })       
+            });
+            };
+        }).then(resultValue=>{
+            console.log(resultValue);
 
+        });
     });
-
-
 };
 
